1cf64f3ecc6dea504fa5a5b95a33acfb339907a9
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2021-01-06 14:30:08 +0900

    add Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,53 @@
-export const movies = [
+export let movies = [
   {
-    id: "1",
+    id: 1,
     name: "happiness",
     score: 8,
   },
   {
-    id: "2",
+    id: 2,
     name: "darkside",
     score: 10,
   },
   {
-    id: "3",
+    id: 3,
     name: "wonder woman",
     score: 6,
   },
   {
-    id: "4",
+    id: 4,
     name: "batman",
     score: 4,
   },
   {
-    id: "5",
+    id: 5,
     name: "superman",
     score: 8,
   },
 ];
 
 export const getById = (id) => {
-  return movies.find((movie) => movie.id === String(id));
+  return movies.find((movie) => movie.id === id);
+};
+
+export const addMovie = (name, score) => {
+  movies.sort((a, b) => a.id - b.id);
+
+  const newMovie = {
+    id: movies[movies.length - 1].id + 1,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((el) => el.id !== id);
+  if (cleanedMovies.length < movies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,18 @@
-import { movies, getById } from "./db";
+import { movies, getById, addMovie, deleteMovie } from "./db";
 
 const resolvers = {
   Query: {
     movies: () => movies,
     movie: (_, { id }) => {
       return getById(id);
+    },
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => {
+      return addMovie(name, score);
+    },
+    deleteMovie: (_, { id }) => {
+      return deleteMovie(id);
     },
   },
 };
